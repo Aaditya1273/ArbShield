f55912eb4833fbf8dd655d8c6b67e30e11b65b88
[Aaditya1273] <aadityaiitian1515@gmail.com>
2026-02-12 19:10:07 +0530

    Arbshield

--- env.ts
+++ env.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().url(),
-    NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().url().optional().default("http://localhost:3000"),
+    NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().min(1).optional().default("demo"),
     NEXT_PUBLIC_CHAIN_ID: z.string().default("421614"),
   },
   runtimeEnv: {
